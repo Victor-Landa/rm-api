f6fe5e7df69299f53b631df1e264d6d165306d78
Victor Landa <victorlanda.dev@gmail.com>
2020-02-15 00:46:13 -0600

    404 page

---
+++ src/pages/Error404.js
@@ -0,0 +1,12 @@
+// El nombre debe de ser el mismo con el 
+// que se creo el archivo (buena práctica)
+const Error404 = () => {
+  const view = `
+    <div class="error404">
+      <h2>Error 404</h2>
+    </div>
+  `;
+  return view;
+};
+
+export default Error404;
